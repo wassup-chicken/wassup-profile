3ed1310140dd50fcefa1b6be702d603508db3790
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2025-04-17 21:38:42 -0400

    fix build

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,13 +1,6 @@
-export const dynamic = "force-dynamic";
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 export async function GET() {
     const data = { message: "Hello from the fake server!" };
 
-    await sleep(1000);
-
     return Response.json({ data });
 }
+
