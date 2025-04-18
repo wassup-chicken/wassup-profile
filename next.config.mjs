b22decda09d8451f6ff676a3ec237930c7d2414a
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2025-04-17 21:59:04 -0400

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     basePath: '/wassup-profile',          // ← Set this to your GitHub repo name
     assetPrefix: '/wassup-profile',
 };
