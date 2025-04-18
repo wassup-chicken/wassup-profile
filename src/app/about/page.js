2ffc746f3fda37e58682258063f05115fcced640
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2025-04-17 21:33:59 -0400

    update about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,14 +6,14 @@ const About = () => {
             </svg>
             <div className="flex justify-center items-center mt-10">
                 <p className="w-full p-10">
-                    Hi 👋, <br />
+                    {`Hi 👋, <br />
                     I’m Seung — a software engineer with a strong background in backend development, and enterprise systems.<br />
                     I'm currently working on finance/accounting-related automation and SaaS integrations, and I love building tools that solve real-world workflow pain points.<br /><br />
                     I’m especially interested in: <br /><br />
                     🛠 Building backend services (Golang, Java)<br /><br />
                     🌐 Modern web apps using React, Next.js, Tailwind CSS<br /><br />
                     🤖 Automating workflows and integrating business systems<br /><br />
-                    Outside of coding, I love exploring product ideas, diving into new technologies, playing soccer, and spending quality time with my wonderful wife.
+                    Outside of coding, I love exploring product ideas, diving into new technologies, playing soccer, and spending quality time with my wonderful wife.`}
                 </p>
             </div>
         </>
