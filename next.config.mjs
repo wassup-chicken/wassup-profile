788e654619329ad2f497b90d85dcca411f218a71
Wassup Chicken <74932143+wassup-chicken@users.noreply.github.com>
2025-04-17 21:45:16 -0400

    update config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/wassup-profile',
+    assetPrefix: '/wassup-profile',
+
+};
 
 export default nextConfig;
